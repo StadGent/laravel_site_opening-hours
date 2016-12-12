ab4450562e52cb2b2574f42c9e5318e64b8f0cc1
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2016-12-12 17:19:59 +0100

    Added dummy json as a response

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -13,7 +13,36 @@ class ServicesController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json([
+            [
+                'label' => 'Dienst administratieve vereenvoudiging',
+                'uri' => 'http://stad.gent/vereenvoudiging',
+                'channels' => [
+                    [
+                        'id' => 1,
+                        'label' => 'telefonisch',
+                        'openinghours' => [
+                            [
+                                'active' => true,
+                                'start_date' => (Carbon::now())->subMonth()->toDateString(),
+                                'end_date' => (Carbon::now())->subMonth()->addYear()->toDateString(),
+                                'id' => 5
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            [
+                'label' => 'Bib Zuid',
+                'uri' => 'http://stad.gent/bib-zuid'
+                'channels' => [
+                    'id' => 2,
+                    'label' => 'loket',
+                    'openinghours' => [
+                    ]
+                ]
+            ]
+        ]);
     }
 
     /**
@@ -23,7 +52,7 @@ class ServicesController extends Controller
      */
     public function create()
     {
-        //
+        throw new Exception('Not yet implemented');
     }
 
     /**
@@ -34,51 +63,51 @@ class ServicesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        throw new Exception('Not yet implemented');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int                       $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
+        throw new Exception('Not yet implemented');
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int                       $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        //
+        throw new Exception('Not yet implemented');
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  int                       $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+        throw new Exception('Not yet implemented');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int                       $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        throw new Exception('Not yet implemented');
     }
 }
