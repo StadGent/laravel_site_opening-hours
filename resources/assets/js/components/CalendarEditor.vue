e72aff400c4f12f42a05b2c27ea5e01e18dd14e8
Thomas <info@thomasg.be>
2016-12-21 00:50:03 +0100

    Allow to delete a calendar

--- resources/assets/js/components/CalendarEditor.vue
+++ resources/assets/js/components/CalendarEditor.vue
@@ -39,7 +39,7 @@
 
     <div class="wrapper-save-btn">
       <div class="col-xs-12 text-right">
-        <button type="button" class="btn btn-default pull-left" @click="toVersion()">Verwijder</button>
+        <button type="button" class="btn btn-default pull-left" @click="rmCalendar()">Verwijder</button>
         <button type="button" class="btn btn-default" @click="toVersion()">Annuleer</button>
         <button type="submit" class="btn btn-primary" @click="saveLabel" v-if="cal.label=='Uitzondering'">Volgende stap</button>
         <button type="submit" class="btn btn-primary" @click="save" v-else>Sla op</button>
@@ -99,6 +99,9 @@ export default {
       }
       this.cal.label = this.calLabel
       Hub.$emit('createCalendar', this.cal)
+    },
+    rmCalendar () {
+      Hub.$emit('deleteCalendar', this.cal)
     }
   },
   created () {
