e8e5254dff1074cdd99e15d26652d3d8d1ce78f5
Thomas <info@thomasg.be>
2017-03-02 14:24:17 +0100

    Fix Safari issue

--- resources/assets/js/defaults.js
+++ resources/assets/js/defaults.js
@@ -18,8 +18,8 @@ export function createFirstEvent() {
 export function createEvent(label) {
   return {
     start_date: new Date().toJSON().slice(0, 11) + '00:00:00',
-    end_date: new Date(new Date() + 36e5 * 24).toJSON().slice(0, 11) + '00:00:00',
-    until: new Date(new Date() + 36e5 * 24).toJSON().slice(0, 11) + '00:00:00',
+    end_date: new Date(new Date().valueOf() + 36e5 * 24).toJSON().slice(0, 11) + '00:00:00',
+    until: new Date(new Date().valueOf() + 36e5 * 24).toJSON().slice(0, 11) + '00:00:00',
     rrule: 'FREQ=DAILY',
     label: label || '1'
   }
