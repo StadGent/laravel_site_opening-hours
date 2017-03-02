843ab6642aabf4f4bf18aad460f03f976f43a2f8
Thomas <info@thomasg.be>
2017-03-02 15:09:56 +0100

    Disable open/closed toggle

--- resources/assets/js/page/PageVersion.vue
+++ resources/assets/js/page/PageVersion.vue
@@ -6,7 +6,7 @@
     <div class="pull-right">
       <div class="btn-group">
         <button type="button" class="btn btn-default" :class="{active: !tab}" @click="tab=0">Toon periodes</button>
-        <button type="button" class="btn btn-default" :class="{active: tab=='users'}" @click="tab='users'">Toon open en gesloten</button>
+        <button type="button" class="btn btn-default" :class="{active: tab=='users'}" @click="tab='users'" disabled>Toon open en gesloten</button>
       </div>
     </div>
 
