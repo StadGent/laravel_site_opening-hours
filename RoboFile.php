4d29e1484d2c214a9206d921bbb8ef83039a21e1
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2017-03-20 11:25:03 +0100

    Restore robofile back to basic.

--- RoboFile.php
+++ RoboFile.php
@@ -6,26 +6,5 @@ use DigipolisGent\Robo\Laravel\RoboFileBase;
 
 class RoboFile extends RoboFileBase
 {
-    /**
-     * TODO: remove this method once the gulp task is fixed.
-     */
-    protected function buildTask($archivename = null)
-    {
-        $archive = is_null($archivename) ? $this->time . '.tar.gz' : $archivename;
-        $collection = $this->collectionBuilder();
-        $collection
-            ->taskPackageProject($archive)
-                ->ignoreFileNames([
-                    '.env.example',
-                    '.gitattributes',
-                    '.gitignore',
-                    'README',
-                    'README.txt',
-                    'README.md',
-                    'LICENSE',
-                    'LICENSE.txt',
-                    'LICENSE.md',
-                ]);
-        return $collection;
-    }
+
 }
