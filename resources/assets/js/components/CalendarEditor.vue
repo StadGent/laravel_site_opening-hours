e0a5b0223e3f7e0c809b051fb5e388dd4da4e6e4
Thomas <info@thomasg.be>
2017-03-24 15:04:47 +0100

    Remove cancel button

--- resources/assets/js/components/CalendarEditor.vue
+++ resources/assets/js/components/CalendarEditor.vue
@@ -44,7 +44,6 @@
     <div class="wrapper-save-btn">
       <div class="col-xs-12 text-right">
         <button type="button" class="btn btn-default pull-left" @click="rmCalendar()">Verwijder</button>
-        <button type="button" class="btn btn-default" @click="cancel">Annuleer</button>
         <button type="submit" class="btn btn-primary" @click="saveLabel" v-if="cal.label=='Uitzondering'">Volgende stap</button>
         <button type="submit" class="btn btn-primary" @click="save" v-else>Sla op</button>
       </div>
