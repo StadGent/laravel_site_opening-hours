8cf3392479a0ad1d665fb1219067e06f024c9156
Merlijn Cuypers <merlijn.cuypers@gmail.com>
2017-09-19 13:53:27 +0200

    OPEN-1 init Laravel/Vue passport install

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laratrust\Traits\LaratrustUserTrait;
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
     use LaratrustUserTrait;
     use Notifiable;
 
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,7 +6,22 @@ import { rootRouterMixin } from './mixins/router.js'
 import printmeMixin from './mixins/printme.js'
 import servicesMixin from './mixins/services.js'
 import usersMixin from './mixins/users.js'
+/*
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue')
+);
 
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue')
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue')
+);
+ */
 const app = new Vue({
   el: '#app',
   render: h => h(App),
