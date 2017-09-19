e19732f98f5f723b45499898af92119f4f0779af
Merlijn Cuypers <merlijn.cuypers@gmail.com>
2017-09-19 16:27:58 +0200

    OPEN-2 refactor openinghours events

--- app/Models/Openinghours.php
+++ app/Models/Openinghours.php
@@ -2,8 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Jobs\DeleteLodOpeninghours;
+use App\Jobs\UpdateLodOpeninghours;
+use App\Jobs\UpdateVestaOpeninghours;
+use App\Models\Service;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
 
 class Openinghours extends Model
 {
@@ -19,7 +23,7 @@ class Openinghours extends Model
      * @var array
      */
     protected $fillable = [
-        'active', 'start_date', 'end_date', 'label', 'channel_id'
+        'active', 'start_date', 'end_date', 'label', 'channel_id',
     ];
 
     public function calendars()
@@ -32,10 +36,60 @@ class Openinghours extends Model
         return $this->belongsTo('App\Models\Channel');
     }
 
+    /**
+     * hook into eloquent events
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        /**
+         * check or vesta needs to be updated for this openinghours
+         * updated openinghours that is stored in LOD
+         */
+        static::updated(function ($openinghours) {
+            $channel = $openinghours->channel();
+            dd($channel);
+            $service = $channel->service();
+            self::updateVestaWithOpeninghours($openinghours, $service);
+            dispatch(new UpdateLodOpeninghours($service->id, $openinghours->id, $channel->id));
+        });
+
+        /**
+         * check or vesta needs to be updated for this openinghours
+         * delete openinghours that is stored in LOD
+         */
+        static::deleting(function ($openinghours) {
+            $service = $$openinghours->channel()->service();
+
+            self::updateVestaWithOpeninghours($openinghours, $service);
+            dispatch(new DeleteLodOpeninghours($service->id, $openinghours->id));
+        });
+    }
+
     public function getActiveAttribute()
     {
         $today = Carbon::today()->toDateString();
 
         return $this->start_date <= $today && (empty($this->end_date) || $this->end_date >= $today);
     }
+
+    /**
+     * Update Vesta With Openinghours
+     *
+     * update the VESTA openinghours of the service,
+     * ONLY WHEN that service is linked to a VESTA UID
+     *
+     * @param  Openinghours $openinghours 
+     * @param  Service      $service      
+     */
+    protected static function updateVestaWithOpeninghours(Openinghours $openinghours, Service $service)
+    {
+        if ($openinghours->active) {
+            if (!empty($service) && $service->source == 'vesta') {
+                dispatch((new UpdateVestaOpeninghours($service->identifier, $service->id)));
+            }
+        }
+    }
+
 }
