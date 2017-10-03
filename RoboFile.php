18116a1c87766c81f9d7d3997afe0a4e44729705
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2017-10-03 10:19:20 +0200

    Add precommit hook.

--- RoboFile.php
+++ RoboFile.php
@@ -21,4 +21,41 @@ class RoboFile extends RoboFileBase
             ->exec('php artisan queue:restart');
         return $collection;
     }
+
+    /**
+     * Run Tests.
+     */
+    public function test() {
+        $this->stopOnFail(true);
+        $this->taskPHPUnit()
+            ->option('disallow-test-output')
+            ->option('report-useless-tests')
+            ->option('strict-coverage')
+            ->option('-d error_reporting=-1')
+            ->option('--coverage-clover=build/logs/clover.xml')
+            ->arg('tests')
+            ->run();
+    }
+
+    /**
+     * Install precommit hook.
+     */
+    public function precommitInstall() {
+        // Create the git/hooks symlinks.
+         $files = array_diff(scandir(__DIR__ . '/.git-hooks'), ['..', '.']);
+         foreach ($files as $file) {
+             $this->say(sprintf('Add git hook : %s.', $file));
+
+             // Only if symlink does not exists yet.
+             $to = '.git/hooks/' . $file;
+             if (file_exists($to)) {
+                 $this->say('✔ Already exists.');
+                 continue;
+             }
+             $this->taskFilesystemStack()
+                 ->symlink(__DIR__ . '/.git-hooks/' . $file, $to)
+                 ->run();
+             $this->say('✔ Created symlink.');
+         }
+    }
 }
