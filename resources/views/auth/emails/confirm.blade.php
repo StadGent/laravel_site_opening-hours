27ac98089b59639e24897990765cf12a9c93dc60
Merlijn Cuypers <merlijn.cuypers@gmail.com>
2017-10-24 10:10:37 +0200

    OPEN-35 fix confirm mail

--- resources/views/auth/emails/confirm.blade.php
+++ resources/views/auth/emails/confirm.blade.php
@@ -1,5 +1,28 @@
-@extends('layouts.email') 
-@section('content', $style)
+@extends('layouts.email')
+<?php
+
+$style = [
+    /* Body ------------------------------ */
+
+    'body_action' => 'width: 100%; margin: 30px auto; padding: 0; text-align: center;',
+    'body_sub' => 'margin-top: 25px; padding-top: 25px; border-top: 1px solid #EDEFF2;',
+
+    /* Type ------------------------------ */
+    'anchor' => 'color: #3869D4;',
+    'paragraph' => 'margin-top: 0; color: #74787E; font-size: 16px; line-height: 1.5em;',
+    'paragraph-sub' => 'margin-top: 0; color: #74787E; font-size: 12px; line-height: 1.5em;',
+
+    /* Buttons ------------------------------ */
+    'button' => 'display: block; display: inline-block; width: 200px; min-height: 20px; padding: 10px;
+                 background-color: #3869D4; border-radius: 3px; color: #ffffff; font-size: 15px; line-height: 25px;
+                 text-align: center; text-decoration: none; -webkit-text-size-adjust: none;',
+
+    'button-blue' => 'background-color: #3869D4;',
+
+    'font_family' => 'font-family: Arial, \'Helvetica Neue\', Helvetica, sans-serif;',
+];
+?>
+@section('content')
 <p>Bevestiging van je registratie op het openingsurenplatform</p>
 <table style="{{ $style['body_action'] }}" align="center" width="100%" cellpadding="0" cellspacing="0">
   <tr>
