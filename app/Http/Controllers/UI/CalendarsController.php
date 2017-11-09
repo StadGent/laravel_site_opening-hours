da665b356402d09e80f90b12018175bea295655c
bart <bart.delrue@digipolis.gent>
2017-11-09 16:30:23 +0100

    OPEN-62: Disabled removal of base calendar - frontend.

--- app/Http/Controllers/UI/CalendarsController.php
+++ app/Http/Controllers/UI/CalendarsController.php
@@ -93,6 +93,13 @@ class CalendarsController extends Controller
             );
         }
 
+//        if ($calendar['priority'] === 0) {
+//            return response()->json(
+//                ['message' => 'De basiskalender kan je niet verwijderen.'],
+//                400
+//            );
+//        }
+
         $success = $this->calendars->delete($calendarId);
 
         if ($success) {
