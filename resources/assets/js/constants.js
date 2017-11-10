e24f54e1b8d41e2884f423c3b28b42614283dffd
bart <bart.delrue@digipolis.gent>
2017-11-10 10:37:24 +0100

    OPEN-79: Commented constants.

--- resources/assets/js/constants.js
+++ resources/assets/js/constants.js
@@ -1,3 +1,4 @@
+// row status
 export const SERVICE_NO_CH = 'Geen kanalen';
 export const SERVICE_MISSING_OH = 'Ontbrekende kalender(s)';
 export const SERVICE_INACTIVE_OH = 'Ontbrekende actieve kalender(s)';
@@ -7,12 +8,15 @@ export const SERVICE_MISSING_OH_TOOLTIP = 'Minstens 1 van de kanalen van deze di
 export const SERVICE_INACTIVE_OH_TOOLTIP = 'Alle kanalen hebben een versie maar minstens 1 kanaal heeft geen versie die nu geldt. Een versie geldt niet als deze verlopen is of pas in de toekomst actief wordt.';
 export const SERVICE_COMPLETE_TOOLTIP = 'Alle kanalen hebben minstens een kalenderversie die nu geldig is.';
 
+// roles
 export const MEMBER = 'Lid';
 export const OWNER = 'Eigenaar';
 export const ADMIN = 'Admin';
 
+// API
 export const API_PREFIX = '/api/v1/ui';
 
+// Validation errors
 export const NO_VALID_EMAIL = 'Dit is geen geldig e-mail adres';
 export const CHOOSE_SERVICE = 'Kies een dienst';
 export const OH_INVALID_RANGE = 'Er mogen geen uitzonderingen beginnen voor de start of eindigen na het einde, van de de nieuwe begin/einddatum van de openingsurenversie.' +
