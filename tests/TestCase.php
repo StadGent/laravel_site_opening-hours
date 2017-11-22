28d559b49defad0ef8cff6949443fe22feb299c5
bart <bart.delrue@digipolis.gent>
2017-11-22 11:08:51 +0100

    OPEN-130: Refactored pikaday date handling.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -125,6 +125,9 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
 
         $path = $this->assemblePath($pathArg);
         $this->doRequest($verb, $path, $data);
+        if($this->response->getStatusCode() !== $statusCode ) {
+            \Log::debug($this->decodeResponseJson());
+        }
         $this->seeStatusCode($statusCode);
     }
 }
