bba386ff2f6596d1ca6cb3f7f8e6e964cf619ca8
Merlijn Cuypers <merlijn.cuypers@gmail.com>
2017-11-29 12:09:38 +0100

    OPEN-33: refactor commands

---
+++ app/Console/Commands/BaseCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class BaseCommand extends Command
+{
+    /**
+     * Write a string as error output.
+     *
+     * overwrite parent to make sure errors go to log
+     *
+     * @param  string  $string
+     * @param  null|int|string  $verbosity
+     * @return void
+     */
+    public function error($string, $verbosity = null)
+    {
+        \Log::error($string);
+        parent::error($string, $verbosity);
+    }
+
+    /**
+     * Write a string as info output.
+     *
+     * overwrite parent to make info go to log
+     *
+     * @param  string  $string
+     * @param  null|int|string  $verbosity
+     * @return void
+     */
+    public function info($string, $verbosity = null)
+    {
+        \Log::info($string);
+        parent::info($string, $verbosity);
+    }
+}
