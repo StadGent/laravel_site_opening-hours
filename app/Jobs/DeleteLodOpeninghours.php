b4222c2bdae9bc85e5db90d9804d8bb3f3278594
Merlijn Cuypers <merlijn.cuypers@gmail.com>
2017-12-04 09:52:54 +0100

    OPEN-72 fix build errors

--- app/Jobs/DeleteLodOpeninghours.php
+++ app/Jobs/DeleteLodOpeninghours.php
@@ -11,7 +11,6 @@ use Illuminate\Queue\SerializesModels;
 
 class DeleteLodOpeninghours extends BaseJob implements ShouldQueue
 {
-    use InteractsWithQueue, Queueable, SerializesModels;
 
     /**
      * @var int
