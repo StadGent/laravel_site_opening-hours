709bcb2d82e3f9be7bb36f2806e5c39496a0cc9f
Merlijn Cuypers <merlijn.cuypers@gmail.com>
2017-12-14 15:36:59 +0100

    OPEN-160 fix view

--- resources/views/api/openinghours/multiple_days.blade.php
+++ resources/views/api/openinghours/multiple_days.blade.php
@@ -32,7 +32,7 @@
                                 <span class="openinghours--time-separator">@lang('openinghourApi.UNTIL_HOUR')</span>
                                 <time datetime="{{$hourArr['until']}}">{{$hourArr['until']}}</time>
                                 @if(end($dayInfoObj->hours) != $hourArr)
-                                    <div class="openinghours--times-between">@lang('openinghourApi.AND')</div>'
+                                    <div class="openinghours--times-between">@lang('openinghourApi.AND')</div>
                                 @endif
                             @endforeach
                         </div>
