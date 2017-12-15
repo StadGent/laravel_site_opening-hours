0c5874125692cef9a928e0a9eec56ca934a8b573
PMassoels <massoelspieter@gmail.com>
2017-12-15 10:13:03 +0100

    OPEN-160 : views update

--- resources/views/api/openinghours/month.blade.php
+++ resources/views/api/openinghours/month.blade.php
@@ -26,46 +26,26 @@
             @endfor
 
             @foreach($data[0]['openinghours'] as $dayInfoObj)
-                @if(empty($dayInfoObj->hours))
-                    <div class="openinghours--day openinghours--day-closed openinghours--day-active">
-                        <span>{{ $dayInfoObj->date->day }}</span>
-                        <div class="openinghours openinghours--details openinghours--day-closed">
-                            <div class="openinghours--date">
-                                <span class="openinghours--date-day">{{ $dayInfoObj->date->day }}</span>
-                                <span class="openinghours--date-month">@lang('openinghourApi.'.$dayInfoObj->date->format('F'))</span>
-                            </div>
-                            <div class="openinghours--content">
-                                <div class="openinghours--times">
-                                    <span class="openinghours--status">@lang('openinghourApi.CLOSED')</span>
-                                </div>
-                            </div>
+                <?php $isSameDay = (new \Carbon\Carbon())->isSameDay($dayInfoObj->date);?>
+                <?php $isOpen = !empty($dayInfoObj->hours);?>
+                <div class="openinghours--day openinghours--day-{{$isOpen ? 'open' : 'closed'}} {{$isSameDay ? 'openinghours--day-active' : ''}}">
+                    <span>{{ $dayInfoObj->date->day }}</span>
+                    <div class="openinghours openinghours--details openinghours--day-{{$isOpen ? 'open' : 'closed'}}">
+                        <div class="openinghours--date">
+                            <span class="openinghours--date-day">{{ $dayInfoObj->date->day }}</span>
+                            <span class="openinghours--date-month">@lang('openinghourApi.'.$dayInfoObj->date->format('F'))</span>
                         </div>
-                    </div>
-                @else
-                    <div class="openinghours--day openinghours--day-open openinghours--day-active">
-                        <span>{{ $dayInfoObj->date->day }}</span>
-                        <div class="openinghours openinghours--details openinghours--day-open">
-                            <div class="openinghours--date">
-                                <span class="openinghours--date-day">{{ $dayInfoObj->date->day }}</span>
-                                <span class="openinghours--date-month">@lang('openinghourApi.'.$dayInfoObj->date->format('F'))</span>
-                            </div>
+                        <div class="openinghours--content">
                             <div class="openinghours--times">
-                                <span class="openinghours--status">@lang('openinghourApi.OPEN')</span>
-                                <div class="openinghours--time">
-                                    @foreach($dayInfoObj->hours as $hourArr)
-                                        <span class="openinghours--time-prefix">@lang('openinghourApi.FROM_HOUR')</span>
-                                        <time datetime="{{$hourArr['from']}}">{{$hourArr['from']}}</time>
-                                        <span class="openinghours--time-separator">@lang('openinghourApi.UNTIL_HOUR')</span>
-                                        <time datetime="{{$hourArr['until']}}">{{$hourArr['until']}}</time>
-                                        @if(end($dayInfoObj->hours) != $hourArr)
-                                            <div class="openinghours--times-between">@lang('openinghourApi.AND')</div>
-                                        @endif
-                                    @endforeach
-                                </div>
+                                @if($isOpen)
+                                    @include('api.openinghours.openinghour_times_open',['dayInfoObj' => $dayInfoObj])
+                                @else
+                                    <span class="openinghours--status">@lang('openinghourApi.CLOSED')</span>
+                                @endif
                             </div>
                         </div>
                     </div>
-                @endif
+                </div>
             @endforeach
 
             @for($i=0;$i<((7 - $lastDay->dayOfWeek + $localeService->getWeekEndDay()) % 7);$i++)
