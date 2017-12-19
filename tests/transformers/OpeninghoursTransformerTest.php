20d7c770ad377f169b86f8c26ed15f1fea9e74c1
Peter Decuyper <peter.decuyper@digipolis.gent>
2017-12-19 11:59:17 +0100

    OPEN-160: Changed month test period to avoid failing test during the first 7 days in January.

--- tests/transformers/OpeninghoursTransformerTest.php
+++ tests/transformers/OpeninghoursTransformerTest.php
@@ -88,8 +88,8 @@ class OpeninghoursTransformerTest extends \TestCase
         $transformer = new OpeninghoursTransformer();
         $transformer->setIncludeIsOpenNow(false);
         $transformer->setService($service);
-        $transformer->setStart((new Carbon('2018-01-01'))->startOfDay());
-        $transformer->setEnd((new Carbon('2018-01-07'))->endOfDay());
+        $transformer->setStart((new Carbon('2017-01-01'))->startOfDay());
+        $transformer->setEnd((new Carbon('2017-01-07'))->endOfDay());
         $transformer->setCalendarLength(QueryController::CALENDAR_LENGTH_MONTH);
         $transformer->setLocaleService($this->localeService);
         $actual = response()->collection($transformer, $service->channels)->content();
