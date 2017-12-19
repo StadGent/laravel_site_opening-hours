be23aa0c3d538da228db43c441ef0aaf4a090018
Peter Decuyper <peter.decuyper@digipolis.gent>
2017-12-19 08:59:21 +0100

    OPEN-160: Refactored the Multiple days formatting.

--- resources/views/api/openinghours/multiple_days.blade.php
+++ resources/views/api/openinghours/multiple_days.blade.php
@@ -4,21 +4,12 @@
         @foreach($data[0]['openinghours'] as $dayInfoObj)
             <?php $dayString = \App\Models\DayInfo::WEEKDAYS[$dayInfoObj->date->dayOfWeek]; ?>
             <?php $isOpen = !empty($dayInfoObj->hours); ?>
-            <div property="openingHoursSpecification" typeof="OpeningHoursSpecification" class="openinghours--day openinghours--day-{{$isOpen ? 'open' : 'closed'}}">
+            <div property="openingHoursSpecification" typeof="OpeningHoursSpecification" class="openinghours--day openinghours--day-{{ $isOpen ? 'open' : 'closed' }}">
                 <div class="openinghours--date">
                     <span class="openinghours--date--day-of-week"><link property="dayOfWeek" href="http://schema.org/{{ $dayString }}"/>@lang('openinghourApi.'.$dayString)</span>
                     <time property="validFrom validThrough" datetime="{{ $dayInfoObj->date->toDateString() }}">{{ $dayInfoObj->date->format('d/m/Y') }}</time>
                 </div>
-                <div class="openinghours--times">
-                    <div class="openinghours--time">
-                        @if($isOpen)
-                            @include('api.openinghours.openinghour_times_open',['dayInfoObj' => $dayInfoObj])
-                        @else
-                            <time property="opens" datetime="00:00:00">@lang('openinghourApi.CLOSED')</time>
-                            <time property="closes" datetime="00:00:00"></time>
-                        @endif
-                    </div>
-                </div>
+                @include('api.openinghours.times', ['dayInfoObj' => $dayInfoObj])
             </div>
         @endforeach
     @endif
