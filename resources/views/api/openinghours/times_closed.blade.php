4656eff086d0a6f1e1aee7548a1592a93daa1a59
Peter Decuyper <peter.decuyper@digipolis.gent>
2017-12-20 11:42:19 +0100

    OPN-160: Changed the way the closed status (and time) is rendered.

--- resources/views/api/openinghours/times_closed.blade.php
+++ resources/views/api/openinghours/times_closed.blade.php
@@ -1,5 +1,8 @@
 {{--
   -- Template rendering the times information for a closed day.
   --}}
-<time property="opens" datetime="00:00:00">@lang('openinghourApi.CLOSED')</time>
-<time property="closes" datetime="00:00:00"></time>
+<span class="openinghours--status">@lang('openinghourApi.CLOSED')</span>
+<div class="openinghours--time">
+    <time property="opens" datetime="00:00:00"></time>
+    <time property="closes" datetime="00:00:00"></time>
+</div>
