8a2c84a5e38df44eb9adc00607e20fa03c2c8fc2
Pieter MAssoels <massoelspieter@gmail.com>
2018-01-05 10:14:49 +0100

    OPEN-167 : translations added

--- resources/lang/es/openinghourApi.php
+++ resources/lang/es/openinghourApi.php
@@ -19,6 +19,48 @@ return [
     'UNTILL_DATA' => 'al',
     'SHORT_HOUR' => 'h',
 
+    'day_0'  => 'Domingo',
+    'day_1'  => 'Lunes',
+    'day_2'  => 'Martes',
+    'day_3'  => 'Miércoles',
+    'day_4'  => 'Jueves',
+    'day_5'  => 'Viernes',
+    'day_6'  => 'Viernes',
+
+    'PREVIOUS'   => 'previous',
+    'NEXT' => 'next',
+
     'TODAY' => 'Hoy',
     'TOMORROW' => 'Mañana',
+
+    'Monday' => 'Lunes',
+    'Tuesday' => 'Martes',
+    'Wednesday' => 'Miércoles',
+    'Thursday' => 'Jueves',
+    'Friday' => 'Viernes',
+    'Saturday' => 'Sábado',
+    'Sunday' => 'Domingo',
+
+    'Mo' => 'Lu',
+    'Tu' => 'Ma',
+    'We' => 'Mi',
+    'Th' => 'Ju',
+    'Fr' => 'Vi',
+    'Sa' => 'Sá',
+    'Su' => 'Do',
+
+    'January' => 'enero',
+    'February' => 'febrero',
+    'March' => 'marzo',
+    'April' => 'abril',
+    'May' => 'mayo',
+    'June' => 'junio',
+    'July' => 'julio',
+    'August' => 'agosto',
+    'September' => 'septiembre',
+    'October' => 'octubre',
+    'November' => 'noviembre',
+    'December' => 'diciembre',
+
+    'title' => 'Horario de apertura',
 ];
