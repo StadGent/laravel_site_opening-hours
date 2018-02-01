ecdcaa5965fa4868fe82d85985a6bfe3a3d31f4c
bart <bart.delrue@digipolis.gent>
2018-02-01 14:14:32 +0100

    OPEN-185: Removed set start_date to first occurence of rrule. !FAILED TEST Tests\Console\FetchRecreatexTest::testCalendarIsImported

--- resources/assets/js/components/CalendarEditor.vue
+++ resources/assets/js/components/CalendarEditor.vue
@@ -218,23 +218,9 @@
                 this.$root.fetchVersion(true)
             },
             save() {
-                // Set start_date to first occurrence of rrule
-                this.cal.events.forEach(e => {
-                    const limitedRule = keepRuleWithin(e);
-                    const date = rruleToStarts(limitedRule + ';COUNT=1')[0];
-                    if (date) {
-                        date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
-                        if (e.start_date.slice(0, 10) !== date.toJSON().slice(0, 10)) {
-                            e.start_date = date.toJSON().slice(0, 10) + e.start_date.slice(10);
-                            e.end_date = date.toJSON().slice(0, 10) + e.end_date.slice(10)
-                        }
-                    }
-                });
-
                 if (this.disabled) {
                     return console.warn('Expected valid calendar')
                 }
-
                 Hub.$emit('createCalendar', this.cal, true)
             },
             saveLabel() {
