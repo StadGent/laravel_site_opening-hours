f818dcf108f19913dc81bbffa56760a4e548fecc
Pieter MAssoels <massoelspieter@gmail.com>
2018-02-14 14:01:40 +0100

    OPEN-152 : verbetering vesta output

--- tests/services/RecurringOHServiceTest.php
+++ tests/services/RecurringOHServiceTest.php
@@ -143,13 +143,12 @@ class RecurringOHServiceTest extends \TestCase
 <h3>BALIE</h3>
 <div>
 <h4>Normale uren geldig t.e.m. zondag 31 december 2017</h4>
-<p>maandag tot vrijdag gesloten</p>
-<\div>
+<p>maandag tot vrijdag : gesloten</p>
+</div>
 <div>
 <h4>Normale uren geldig vanaf maandag 1 januari 2018</h4>
-<p>maandag tot vrijdag: 8 tot 12 uur<br />
-maandag tot vrijdag: 13 tot 17 uur</p>
-<\div>
+<p>maandag tot vrijdag : 8 tot 12 uur en 13 tot 17 uur</p>
+</div>
 EOL;
 
         $expected = str_replace(PHP_EOL,'',$expected);
@@ -204,8 +203,8 @@ EOL;
 <h3>BALIE</h3>
 <div>
 <h4>Normale uren geldig t.e.m. zondag 31 december 2017</h4>
-<p>maandag tot vrijdag gesloten</p>
-<\div>
+<p>maandag tot vrijdag : gesloten</p>
+</div>
 EOL;
 
         $expected = str_replace(PHP_EOL,'',$expected);
