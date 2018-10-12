347c415a157eeb370a1164d34cb84b431b759e67
bart <bart.delrue@digipolis.gent>
2018-10-12 14:40:55 +0200

    OPEN-231: Removed dummy channel type output from widget.

--- resources/views/api/openinghours/day.blade.php
+++ resources/views/api/openinghours/day.blade.php
@@ -7,7 +7,6 @@
             {{ $channelData['channel'] }}
         </div>
     @endif
-    {{ $channelData['channelType'] }}
     {{--Looping over all openinghours objects--}}
     @foreach($channelData['openinghours'] as $dayInfoObj)
         @include('api.openinghours.day_info', ['dayInfoObj' => $dayInfoObj])
