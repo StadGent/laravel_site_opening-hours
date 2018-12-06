69d82dd59e68a78fd23718fd586fe49f8a204b50
bart <bart.delrue@digipolis.gent>
2018-12-06 13:20:45 +0100

    OPEN-133: Added channel soft delete.

--- app/Models/Channel.php
+++ app/Models/Channel.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Channel extends Model
 {
+
+    use SoftDeletes;
+
     /**
      * The attributes that are mass assignable.
      *
