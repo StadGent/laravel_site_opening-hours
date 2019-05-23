53cbe4860707ecdf814fc738d329f7b596de7796
bart <bart.delrue@digipolis.gent>
2019-05-23 15:18:53 +0200

    OPEN-266: Fixed incorrect end_date when copying versions past midnight.

--- app/Models/Openinghours.php
+++ app/Models/Openinghours.php
@@ -2,14 +2,18 @@
 
 namespace App\Models;
 
-use App\Models\Ical;
 use Carbon\Carbon;
+use DateTime;
+use DateInterval;
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 
 class Openinghours extends Model
 {
+
     /**
      * The table to store the openinghours in
+     *
      * @var string
      */
     protected $table = 'openinghours';
@@ -25,7 +29,11 @@ class Openinghours extends Model
      * @var array
      */
     protected $fillable = [
-        'active', 'start_date', 'end_date', 'label', 'channel_id',
+        'active',
+        'start_date',
+        'end_date',
+        'label',
+        'channel_id',
     ];
 
     /**
@@ -92,11 +100,13 @@ class Openinghours extends Model
 
     /**
      * Copy calendars and events from another version.
+     *
      * @param $originalVersion
      *
      * @return $this
      */
-    public function copy($originalVersion) {
+    public function copy($originalVersion)
+    {
         foreach (Openinghours::find($originalVersion)->calendars->toArray() as $calendar) {
             $calendar['openinghours_id'] = $this->id;
             $calendar['published'] = false;
@@ -105,9 +115,34 @@ class Openinghours extends Model
             foreach (Calendar::find($calendar['id'])->events->toArray() as $event) {
                 // update period of the base openinghours
                 if ($new_calendar->priority === 0) {
-                    // update date, keep time
-                    $event['start_date'] = $this->start_date . ' ' . explode(' ', $event['start_date'])[1];
-                    $event['end_date'] = $this->start_date . ' ' . explode(' ', $event['end_date'])[1];
+
+                    // in case of opening hours past midnight, end date is next day
+                    // we need to keep the difference in days between start and end date
+                    try {
+                        // get the difference in days
+                        $start_date = new DateTime($event['start_date']);
+                        $end_date = new DateTime($event['end_date']);
+                        $start_date->setTime(0, 0, 0, 0);
+                        $end_date->setTime(0, 0, 0, 0);
+                        $days_difference = $start_date->diff($end_date)->days;
+
+                        // add the same difference ot the current start_date
+                        $end_date = (new DateTime($this->start_date))
+                            ->add(new DateInterval('P' . $days_difference . 'D'))
+                            ->format('Y-m-d');
+
+                        // update date, keep time
+                        $event['end_date'] = $end_date . ' ' . explode(' ',
+                                $event['end_date'])[1];
+                    } catch (Exception $e) {
+                        // just in case some date value was malformed, keep the old behaviour
+                        $event['end_date'] = $this->start_date . ' ' . explode(' ',
+                                $event['end_date'])[1];
+                    } finally {
+                        // update date, keep time
+                        $event['start_date'] = $this->start_date . ' ' . explode(' ',
+                                $event['start_date'])[1];
+                    }
 
                     // update until to end_date of the version
                     $event['until'] = $this->end_date;
