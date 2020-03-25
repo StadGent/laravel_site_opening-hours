d3756901ad1ed2b261c5b48e06bfbca223441b46
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2020-03-25 11:29:36 +0100

    New file locations for holidays.

--- config/app.php
+++ config/app.php
@@ -132,10 +132,10 @@ return [
     ],
 
     'supported_exception_callendars' => [
-        'feestdagen' => 'https://data.stad.gent/sites/default/files/feestdagen-nl-BE.ical',
-        'schoolvakanties' => 'https://data.stad.gent/sites/default/files/schoolvakanties-nl-BE.ical',
-        'gentsefeesten' => 'https://data.stad.gent/sites/default/files/gentsefeesten-nl-BE.ical',
-        'extra-stad-gent' => 'https://data.stad.gent/sites/default/files/extra-feestdagen-stad-gent-nl-BE.ical',
+        'feestdagen' => 'https://datafiles.stad.gent/sites/default/files/feestdagen-nl-BE.ical',
+        'schoolvakanties' => 'https://datafiles.stad.gent/sites/default/files/schoolvakanties-nl-BE.ical',
+        'gentsefeesten' => 'https://datafiles.stad.gent/sites/default/files/gentsefeesten-nl-BE.ical',
+        'extra-stad-gent' => 'https://datafiles.stad.gent/sites/default/files/extra-feestdagen-stad-gent-nl-BE.ical',
     ],
 
     /*
