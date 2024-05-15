731bccb9f384889660bfdbc9bf4589becfce2314
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2024-05-15 14:09:30 +0200

    Fix code style

--- app/Csp/Policies/OpeningHours.php
+++ app/Csp/Policies/OpeningHours.php
@@ -6,12 +6,14 @@ use Spatie\Csp\Directive;
 use Spatie\Csp\Keyword;
 use Spatie\Csp\Policies\Basic;
 
-class OpeningHours extends Basic {
+class OpeningHours extends Basic 
+{
 
     /**
      * {@inheritdoc}
      */
-    public function configure() {
+    public function configure() 
+    {
         parent::configure();
 
         $this->addDirective(Directive::SCRIPT, 'https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.3.0/zxcvbn.js');
