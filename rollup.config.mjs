e0dfdea1c8a6a4c5e66cfe0f7e5cbd96f336f92b
Jonas <jonas@Jonass-MacBook-Pro.local>
2024-07-08 15:12:11 +0200

    Add and update packages and update rollup config
    Rollup/plugin-commonjs
    rollup/plugin-node-resolve
    commonjs
    rollup-plugin-commonjs
    vue-multiselect
    devdependency:
    rollup-plugin-css-only

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,25 +1,29 @@
 import vue2 from 'rollup-plugin-vue2'
 import buble from 'rollup-plugin-buble'
-// import nodeResolve from 'rollup-plugin-node-resolve'
-// import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs';
+import css from 'rollup-plugin-css-only'
 
 export default {
   input: 'resources/assets/js/app.js',
   output: {
-      file: 'public/js/bundle.js',
-      format: 'es',
-      sourcemap: true
+    file: 'public/js/bundle.js',
+    format: 'es',
+    sourcemap: true
   },
   watch: {
-      chokidar: false,
-      include: 'resources/assets/*/**'
+    chokidar: false,
+    include: 'resources/assets/*/**'
   },
   plugins: [
+    css(),
     vue2(),
     buble(),
-    // nodeResolve(),
-    // commonjs({
-    //   include: 'node_modules/**'
-    // })
+    nodeResolve({
+      browser: true,
+    }),
+    commonjs({
+      include: 'node_modules/**'
+    })
   ]
 }
