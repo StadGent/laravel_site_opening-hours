43ece66a74b33a0e162d6d612f7a667647263e8c
kobedev <kobe.deville@district09.gent>
2026-02-20 11:53:13 +0100

    Upgrade to Laravel12: migrations
    Added migrations for OAuth_device_codes
    and the necessary provider column for passport 13 support

---
+++ database/migrations/2026_02_20_102644_add_provider_to_oauth_clients.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('oauth_clients', function (Blueprint $table) {
+            // Add 'provider' column that Passport 13 requires for socialite providers, but make it nullable since we don't use it yet
+            $table->string('provider')->after('secret')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('oauth_clients', function (Blueprint $table) {
+            $table->dropColumn('provider');
+        });
+    }
+};
